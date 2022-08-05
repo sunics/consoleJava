87cdbfa52bf0f8719cb2332169e250d31bd12ed3
sunics <sunics@gmail.com>
2022-08-05 15:08:06 +0530

    student updated

--- src/consoleJava/Student.java
+++ src/consoleJava/Student.java
@@ -4,7 +4,7 @@ public class Student {
 
 	public static void main(String[] args) {
 		System.out.println("Hello Students");
-		System.out.println("I Love Java");
+		System.out.println("I Love Java Programming");
 
 	}
 
